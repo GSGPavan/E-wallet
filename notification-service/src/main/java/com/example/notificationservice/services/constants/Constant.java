59b666f28eb6deae22a8ca3dccff3569594530cf
GSGPavan <108477588+GSGPavan@users.noreply.github.com>
2023-09-20 23:48:43 +0530

    Delete notification-service directory

--- notification-service/src/main/java/com/example/notificationservice/services/constants/Constant.java
+++
@@ -1,8 +0,0 @@
-package com.example.notificationservice.services.constants;
-
-public class Constant {
-
-    public static final String WALLET_CREATED_TOPIC = "WALLET_CREATED";
-
-    public static final String TRANSACTION_UPDATED_TOPIC = "TRANSACTION_UPDATED";
-}
